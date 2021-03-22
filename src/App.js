750e540de4e86f8ec31e7dede89e8aca3c4d5824
dukim <dukim@bnpinnovation.com>
2021-03-22 19:19:26 +0900

    ch04 작성중

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Say from './ch03/Say2';
+import Say from './ch04/ScrollBox';
+import ScrollBox from './ch04/ScrollBox';
 
 function App() {
-  return <Say />;
+  return <ScrollBox />;
 }
 
 export default App;
